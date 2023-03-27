16c60449a5b4619d7e27f2ae521c8842e6c8e4ad
karmen <k.yousfan@gopaderd.org>
2023-03-27 18:57:39 +0300

    AddManifest

--- OfflineTest/Startup.cs
+++ OfflineTest/Startup.cs
@@ -37,6 +37,7 @@ namespace OfflineTest
             services.AddControllersWithViews();
 
             services.AddServices();
+            services.AddProgressiveWebApp();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
